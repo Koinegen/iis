bb3b59b55b52d1137f994fc31cc6f832e1b84c79
koinegen <1337kng1337@gmail.com>
2020-12-05 19:43:25 +0300

    Доработан основной файл с логикой, убран весь мусорный код

--- lib/main_worker.py
+++ lib/main_worker.py
@@ -1,66 +1,40 @@
-from db_connector import DataBase
+from lib.db_connector import DataBase
 import json
 import random
 from collections import defaultdict
 
 
 class TestSession:
-    def __init__(self, db: DataBase, cookies):
+    def __init__(self, db: DataBase):
         self.db = db
-        self.cookies = cookies
-        self.properties = {}
         self.answers = []
         self.result = []
-        self.languages = {}
         self.sorted_property_list = []
         self.last_prop = None
         self.used_prop = []
-        self.properties_count = None
-        # self.__get_sorted_properties()
-        #self._get_all_properties()
-        #self._get_all_langs()
+        self.properties_count = self.db.get_services_count()
 
     def accept_answer(self, ans_num):
         if ans_num == 1:
             self.answers.append(self.last_prop)
 
     def check_results(self):
-        self.result = self.db.get_langs_by_list_of_properties(self.answers)
+        # TODO: Подумать над этой функцией!!!!
+        self.result = self.db.get_services_by_list_of_properties(self.answers)
         if len(self.result) == 1:
-            return f"Вам определенно стоит попробовать {self.result[0]}!"
-        if (len(self.properties) > 0) and (len(self.result) == 0):
+            __result_service = self.db.get_service_by_id(self.result[0])
+            return f"Вам определенно стоит попробовать {__result_service}!"
+        if (len(self.answers) > 0) and (len(self.result) == 0):
             return f"Поздравляю, вы меня победили, можете гордится собой, однако про язык вы ничего не узнаете"
-        elif len(self.properties) == 0:
+        elif len(self.answers) == 0:
             if len(self.result) == 2:
-                return f"Вам стоит присмотреться к этим двум языкам или даже их связке: {self.result[0]} и {self.result[1]}"
-            elif self.result > int(self.properties_count / 2):
+                __result_service1 = self.db.get_service_by_id(self.result[0])
+                __result_service2 = self.db.get_service_by_id(self.result[1])
+                return f"Вам стоит присмотреться к этим двум языкам или даже их связке: {__result_service1} и {__result_service2}"
+            elif len(self.result) > int(self.properties_count / 2):
                 return f"Вы вообще уверены что хотите заниматься программированием?"
-            elif self.result < int(self.properties_count / 2):
-                return f"Вам стоит быть конкретнее, на данный момент вам подходят все вот эти языки: {', '.join(self.result)}"
-            else:
-                print('Some problem here')
-                raise Exception
         else:
             return False
-
-    def _accept_languages(self):
-        for language in self.languages.items():
-            accept = True
-            for prop in self.answers:
-                if language[1].get(prop) == 0:
-                    accept = False
-                elif language[1].get(prop) == 1:
-                    pass
-                else:
-                    print(f"lan = {language}\n"
-                          f"prop = {prop}")
-                    raise Exception
-            if accept:
-                if language[0] not in self.result:
-                    self.result.append(language[0])
-            else:
-                if language[0] in self.result:
-                    self.result.remove(language[0])
 
     def __remove_property(self):
         for i in self.sorted_property_list:
@@ -83,7 +57,10 @@ class TestSession:
         if self.last_prop is not None:
             self.__remove_property()
         self.sorted_property_list = list(filter(lambda x: True if x[1] != [] else False, self.sorted_property_list))
-        prop_id = self.sorted_property_list[0][1][random.randint(0, len(self.sorted_property_list[0][1]) - 1)]
+        try:
+            prop_id = self.sorted_property_list[0][1][random.randint(0, len(self.sorted_property_list[0][1]) - 1)]
+        except IndexError:
+            return False
         questions = self.db.get_question_by_property(prop_id).get('questions')
         self.last_prop = prop_id
         self.used_prop.append(prop_id)
@@ -93,7 +70,7 @@ class TestSession:
 if __name__ == '__main__':
     db = DataBase("../data/katia_bd.db")
 
-    sess = TestSession(db, "123")
+    sess = TestSession(db)
     print(sess.get_question())
-    # sess.accept_answer(1)
-    print(sess.get_question())
+    #sess.accept_answer(1)
+
