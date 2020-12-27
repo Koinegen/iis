876c3bc9f91c26e82ba95253cab184f7d7a93781
kng <1337kng1337@gmail.com>
2020-12-27 14:58:55 +0300

    Исправлен интерфейс

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ from lib.db_connector import DataBase
 
 db = DataBase("./data/lena_db")
 test_session = main_worker.TestSession(db)
-# db = DataBase("./data/lena_db")
 
 
 def result(result_text):
+    btn_da.pack_forget()
+    btn_net.pack_forget()
     lbl.configure(text=f"{result_text}")
-    lbl.grid(column=1, row=1)
-    tk.Button(width=20, height=2, text="Выход", command=lambda: exit(0)).grid(column=1, row=2)
-    tk.Button(width=20, height=2, text="Еще раз?", command=lambda: again()).grid(column=1, row=3)
+    lbl.pack(side=tk.TOP)
+    btn_exit.configure(width=20, height=2, text="Выход", command=lambda: exit(0))
+    btn_exit.pack(side=tk.BOTTOM)
+    btn_again.configure(width=20, height=2, text="Еще раз?", command=lambda: again())
+    btn_again.pack(side=tk.BOTTOM)
 
 
 def again():
     global test_session
     test_session = main_worker.TestSession(db)
-    question()
+    start()
 
 
 def check_results(answer):
@@ -34,23 +37,34 @@ def question():
     if not __question:
         result("ne udalos'")
     lbl.configure(text=__question)
-    tk.Button(width=20, height=2, text='Да', command=lambda: check_results(1)).grid(column=1, row=2)
-    tk.Button(width=20, height=2, text='Нет', command=lambda: check_results(0)).grid(column=1, row=3)
-    # __result = self.test_session.check_results()
-    # if __result:
-    #     self.result(__result)
+    btn_str.destroy()
 
 
+def start():
+    btn_exit.pack_forget()
+    btn_again.pack_forget()
+    btn_da.configure(width=20, height=2, text='Да', command=lambda: check_results(1))
+    btn_da.pack(side=tk.LEFT)
+    btn_net.configure(width=20, height=2, text='Нет', command=lambda: check_results(0))
+    btn_net.pack(side=tk.RIGHT)
+    question()
 
 
 if __name__ == '__main__':
     window = tk.Tk()
     window.title("Экспертная система")
-    lbl = tk.Label(window, width=44, height=5, text="""Вас приветствует экспертна система,
+    window.geometry('600x400')
+    lbl = tk.Label(window, width=80, height=6, text="""Вас приветствует экспертна система,
          которая поможет вам с выбором линукс дистрибутива,
           который подходит именно вам\n
-          Чтобы начать тест нажмите 'Начать'""")
-    lbl.grid(column=1, row=1)
+          Чтобы начать тест нажмите 'Начать'""", background="#555", foreground="#ccc")
+    lbl.pack(side=tk.TOP)
     print(lbl)
-    tk.Button(width=43, height=3, text="Начать!", command=lambda: question()).grid(column=1, row=2)
+    btn_da = tk.Button()
+    btn_net = tk.Button()
+    btn_exit = tk.Button()
+    btn_again = tk.Button()
+    btn_str = tk.Button(width=43, height=3, text="Начать!", command=lambda: start(), background="#555", foreground="#ccc",
+              padx="15", pady="6", font="15")
+    btn_str.pack(side=tk.BOTTOM, fill=tk.Y)
     window.mainloop()
